3726db4aa55906e28c1eb4fb2c944dbd19e8ef8e
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2023-02-26 22:22:49 -0500

    added another algo to solved archive

---
+++ middleOfList.js
@@ -0,0 +1,17 @@
+// this.val = (val===undefined ? 0 : val)
+// this.next = (next===undefined ? null : next) 
+
+
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+
+    
+    
+};
